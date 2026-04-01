428dd1648766f98989cacb654648ab27e746a1bd
Ignacy Dudziak <ignacydudziak@gmail.com>
2026-04-01 15:39:29 +0200

    Lab-5

---
+++ ZaawansowaneBazyDanych/SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    [ProductID] INT                                  NOT NULL,
+    [Attribute] XML(CONTENT [dbo].[AttributeSchema]) NULL,
+    PRIMARY KEY CLUSTERED ([ProductID] ASC),
+    CONSTRAINT [FK_SPA] FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
